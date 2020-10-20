74ae5bf4be641e5a89597d730ac6472a582feb83
zhangy-excellent <250544165@qq.com>
2020-10-20 22:05:06 +0800

    测试ignore

--- web进阶/settings.py
+++ web进阶/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+# 123
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
